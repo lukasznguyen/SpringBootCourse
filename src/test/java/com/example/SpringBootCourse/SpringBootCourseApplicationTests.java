521e47273955a0230255cc3d5c019d39903d21f3
Tuan Viet <lukasznguyen1999@gmail.com>
2021-03-03 11:23:36 +0100

    Generated project from Spring Initializr

---
+++ src/test/java/com/example/SpringBootCourse/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
